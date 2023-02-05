5256d8124574bef48f3cb88d2ecc1990bb448286
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-05 16:21:34 +0530

    Check attendance

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EmployeeWageProblem
+﻿using EmployeeWage;
+
+namespace EmployeeWageProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            EmployeeCheck.CheckAttendence();
         }
+        
+            
     }
 }
