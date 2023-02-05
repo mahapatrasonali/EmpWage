d55fd9acedf76ba2c0cf830e77d0a3226adde67a
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-05 17:00:18 +0530

    20 working Days

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,30 +11,44 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         public static void CheckAttendence()
         {
-
+            int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(2);
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
 
-            switch (empCheck)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0,3);
+
+
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+
+
+
+                Console.WriteLine("EmpWage: " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage: " + empWage);
+            Console.WriteLine("totalEmpwage:" + totalEmpWage);
         }
+
     }
-    
 }
