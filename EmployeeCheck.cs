50c2b5cd948238aed5244538562857aa2f669847
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-05 16:30:20 +0530

    Calculate Emp Wage

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,6 +11,10 @@ namespace EmployeeWage
         public static void CheckAttendence()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
@@ -18,11 +22,15 @@ namespace EmployeeWage
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp wage :" +empWage);
         }
     }
 }
