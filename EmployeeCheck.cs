0d5081ef326aba4fa8d0265fecd5b329ded536da
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-05 17:06:57 +0530

    Calculate the wages

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -12,18 +12,20 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
         public static void CheckAttendence()
         {
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 
             {
+                totalWorkingDays++;
                 Random random = new Random();
-                int empCheck = random.Next(0,3);
+                int empCheck = random.Next(3);
 
 
 
@@ -33,22 +35,24 @@ namespace EmployeeWage
                         empHrs = 4;
                         break;
                     case IS_FULL_TIME:
-                        empHrs = 8;
+                        empHrs = 4;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-
-
-
-                Console.WriteLine("EmpWage: " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("totalEmpwage:" + totalEmpWage);
         }
-
     }
 }
+
+
+
+
+
+        
